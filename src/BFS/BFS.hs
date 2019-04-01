1b5100a389aad47275484e4c3bb1003ca9702bfd
adrian <thechouzanone@gmail.com>
2019-04-01 02:21:12 -0700

    Removed bfsForest as it might be wrong and bfsTree still needs to be fixed.

--- src/BFS/BFS.hs
+++ src/BFS/BFS.hs
@@ -1,7 +1,7 @@
 module BFS (
     --Algorithms
-    bfsAdjacencyMapTree,
-    bfsAdjacencyMapForest,
+    bfsAdjacencyTreeMap,
+    bfsForestAdjacencyMap,
     bfsTree,
     bfsForest
 ) where
@@ -13,23 +13,25 @@ import qualified Data.Set as Set
 import Data.Tree
 import Data.Maybe
 
-bfsForest :: Ord a => AdjacencyMap a -> Forest a
-bfsForest g = map (bfsTree (firstV g)) (bfsAdjacencyMapForest g)
-    where firstV = head . vertexList
+-- bfsForest :: Ord a => AdjacencyMap a -> Forest a
+-- Might implement this one by mapping bfsForestAdjacencyMap
+
 
 bfsTree :: Ord a => a -> AdjacencyMap a -> Tree a
 bfsTree s g = unfoldTree look s
     where look b = (b, (Set.toAscList . fromJust . Map.lookup b) bfs)
-          bfs = adjacencyMap (bfsAdjacencyMapTree s g)
+          bfs = adjacencyMap (bfsAdjacencyTreeMap s g)
 
-bfsAdjacencyMapForest :: Ord a => AdjacencyMap a -> [AdjacencyMap a]
-bfsAdjacencyMapForest g
+bfsForestAdjacencyMap :: Ord a => AdjacencyMap a -> [AdjacencyMap a]
+bfsForestAdjacencyMap g
     | isEmpty g = []
-    | otherwise = headTree : bfsAdjacencyMapForest (induce (\x -> not (hasVertex x headTree)) g)
-        where headTree = bfsAdjacencyMapTree ((head . vertexList) g) g
+    | otherwise = headTree : bfsForestAdjacencyMap (induce (\x -> not (hasVertex x headTree)) g)
+        where headTree = bfsAdjacencyTreeMap ((head . vertexList) g) g
 
-bfsAdjacencyMapTree :: Ord a => a -> AdjacencyMap a -> AdjacencyMap a
-bfsAdjacencyMapTree s g = if (hasVertex s g) then bfsTreeUtil [s] (Set.singleton s) g else empty
+bfsAdjacencyTreeMap :: Ord a => a -> AdjacencyMap a -> AdjacencyMap a
+bfsAdjacencyTreeMap s g = if (hasVertex s g) 
+                          then bfsTreeUtil [s] (Set.singleton s) g 
+                          else empty
 
 bfsTreeUtil :: Ord a => [a] -> Set.Set a -> AdjacencyMap a -> AdjacencyMap a
 bfsTreeUtil [] _ _ = empty
