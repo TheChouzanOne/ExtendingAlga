60ef3b23091e6528db666746a258688250c66251
adrian <thechouzanone@gmail.com>
2019-04-01 01:28:17 -0700

    Added horrible functions for bfsTree and bfsForest. There is an error as bfsTree called upon an empty tree will return the node passed, defeating the purpose of Alga.

--- src/BFS/BFS.hs
+++ src/BFS/BFS.hs
@@ -1,15 +1,35 @@
 module BFS (
     --Algorithms
-    bfsTree
+    bfsAdjacencyMapTree,
+    bfsAdjacencyMapForest,
+    bfsTree,
+    bfsForest
 ) where
 
 import Algebra.Graph.AdjacencyMap.Internal
 import Algebra.Graph.AdjacencyMap
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
+import Data.Tree
+import Data.Maybe
 
-bfsTree :: Ord a => a -> AdjacencyMap a -> AdjacencyMap a
-bfsTree s g = if (hasVertex s g) then bfsTreeUtil [s] (Set.singleton s) g else empty
+bfsForest :: Ord a => AdjacencyMap a -> Forest a
+bfsForest g = map (bfsTree (firstV g)) (bfsAdjacencyMapForest g)
+    where firstV = head . vertexList
+
+bfsTree :: Ord a => a -> AdjacencyMap a -> Tree a
+bfsTree s g = unfoldTree look s
+    where look b = (b, (Set.toAscList . fromJust . Map.lookup b) bfs)
+          bfs = adjacencyMap (bfsAdjacencyMapTree s g)
+
+bfsAdjacencyMapForest :: Ord a => AdjacencyMap a -> [AdjacencyMap a]
+bfsAdjacencyMapForest g
+    | isEmpty g = []
+    | otherwise = headTree : bfsAdjacencyMapForest (induce (\x -> not (hasVertex x headTree)) g)
+        where headTree = bfsAdjacencyMapTree ((head . vertexList) g) g
+
+bfsAdjacencyMapTree :: Ord a => a -> AdjacencyMap a -> AdjacencyMap a
+bfsAdjacencyMapTree s g = if (hasVertex s g) then bfsTreeUtil [s] (Set.singleton s) g else empty
 
 bfsTreeUtil :: Ord a => [a] -> Set.Set a -> AdjacencyMap a -> AdjacencyMap a
 bfsTreeUtil [] _ _ = empty
--- src/BFS/bfsTest.hs
+++ src/BFS/bfsTest.hs
@@ -1,9 +1,16 @@
 import BFS
 import Algebra.Graph.AdjacencyMap
+import Data.Tree
 main = do
     let g = overlays [1*2, 1*3, 1*4, 2*4, 3*5, 4*6, 5*6] :: AdjacencyMap Int
     -- let g = overlays [1*2, 1*3, 2*4, 4*3] :: AdjacencyMap Int
-    putStrLn $ show g
-    putStrLn $ show $ bfsTree 1 g
-    putStrLn $ show $ bfsTree 2 (1*(2+3) + 3*(6+7)+ 2*(4+5)+6*2+2*1+(4+2)*7 :: AdjacencyMap Int)
-    putStrLn $ show $ bfsTree 8 (1*(2+3) + 3*(6+7)+ 2*(4+5)+6*2+2*1+(4+2)*7 :: AdjacencyMap Int)
+    let g2 = 1*(2+3) + 3*(6+7)+ 2*(4+5)+6*2+2*1+(4+2)*7 :: AdjacencyMap Int
+    let g3 = 1 + 2*3 + 4*5*6 :: AdjacencyMap Int
+    -- putStrLn $ show g
+    -- putStrLn $ show $ bfsAdjacencyMapTree 1 g
+    -- putStrLn $ show $ bfsAdjacencyMapTree 2 g2
+    -- putStrLn $ show $ bfsAdjacencyMapTree 8 g2
+    -- putStrLn $ drawTree $ fmap show (bfsTree 2 g2)
+    -- putStrLn $ drawTree $ fmap show (bfsTree 8 g2)
+    putStrLn $ drawTree $ fmap show (bfsTree 2 (2*3 :: AdjacencyMap Int))
+    putStrLn $ show $ bfsAdjacencyMapForest g3
