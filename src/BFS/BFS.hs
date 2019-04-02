1737ed3d527d75c69f0a31d5cb7d5ed18d2272ba
adrian <thechouzanone@gmail.com>
2019-04-02 14:52:15 -0700

    Cleaning up the definitions

--- src/BFS/BFS.hs
+++ src/BFS/BFS.hs
@@ -9,18 +9,18 @@ import Algebra.Graph.AdjacencyMap
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import Data.Tree
-import Data.Maybe
 
 bfsTree :: Ord a => a -> AdjacencyMap a -> Tree a
-bfsTree s g = unfoldTree look s
-    where look b = (b, (Set.toAscList . fromJust . Map.lookup b) bfs)
-          bfs = adjacencyMap (bfsTreeAdjacencyMap s g)
+bfsTree s g = unfoldTree neighbors s
+    where neighbors b = (b, Set.toAscList . postSet b $ bfs)
+          bfs = bfsTreeAdjacencyMap s g
 
 bfsForest :: Ord a => AdjacencyMap a -> [Tree a]
 bfsForest g
     | isEmpty g = []
-    | otherwise = headTree : bfsForest (induce (\x -> not (elem x (flatten headTree))) g)
+    | otherwise = headTree : (bfsForest . induce remove) g
         where headTree = bfsTree ((head . vertexList) g) g
+              remove x = not $ elem x $ flatten headTree
 
 bfsTreeAdjacencyMap :: Ord a => a -> AdjacencyMap a -> AdjacencyMap a
 bfsTreeAdjacencyMap s g = if (hasVertex s g) 
