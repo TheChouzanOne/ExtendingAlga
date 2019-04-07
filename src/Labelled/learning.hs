ae500a8e221bba204c1a9673633290656940f0f6
adrian <thechouzanone@gmail.com>
2019-04-07 16:12:31 -0700

    Playing around with edge sorting.

--- src/Labelled/learning.hs
+++ src/Labelled/learning.hs
@@ -8,6 +8,8 @@ import qualified Data.Map.Strict                     as Map
 import qualified Data.Set                            as Set
 import qualified Algebra.Graph.AdjacencyMap          as AM
 import qualified Algebra.Graph.AdjacencyMap.Internal as AMI
+import Data.Monoid
+import Data.List
 
 sumWeights :: (Num e, Ord a) => AdjacencyMap e a -> e
 sumWeights g = foldr (+) 0 (map (\(x,_,_) -> x) (edgeList g))
@@ -16,3 +18,6 @@ main = do
     let g = overlays [ edge 3 1 2, edge 4 1 5, edge 4 2 3, vertex 6 ] :: AdjacencyMap (Distance Int) Int
     putStrLn $ show $ sumWeights g
     putStrLn $ show $ sort (edgeList g)
+
+getSortedEdges :: (Monoid e, Ord a, Ord e) => AdjacencyMap e a -> [(e,a,a)]
+getSortedEdges = (sortBy (\(w1,_,_) (w2,_,_) -> if w1 < w2 then LT else GT) . edgeList)
